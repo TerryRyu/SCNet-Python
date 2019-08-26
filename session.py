436c274f59e7c5da37423efb0f227aa9d9d685cf
TerryRyu <terry@ijoon.net>
2019-08-26 12:28:24 +0900

    Update session.py
    - Add Peer class
    
    - Add send function

--- session.py
+++ session.py
@@ -6,6 +6,8 @@ import varint
 from lkcp import KcpObj
 
 from message_header import MessageType, MAGIC_PACKET
+from message_wrapper import MessageWrapper
+from utils import get_current_time_sec
 
 
 class BaseSession:
@@ -26,31 +28,18 @@ class RendezvousSession(BaseSession):
         self.connection_id = connection_id
         self.crypto_type = 0
 
+        self.private_kcp_peer = None
         self.public_kcp_peer = None
+        self.relay_kcp_peer = None
 
-    def is_connected():
-        return True
+    def is_connected(self):
+        if self.private_kcp_peer is not None \
+                or self.public_kcp_peer is not None \
+                or self.relay_kcp_peer is not None:
 
-    def set_public_kcp_peer(self, ip, port):
-        self.public_kcp_peer = KcpPeer(self.sock, ip, port)
+            return True
 
-    def send(message_wrapper):
-
-        coded_output = b''
-
-        coded_output += MAGIC_PACKET
-        coded_output += varint.encode(message_wrapper.byte_size)
-        coded_output += varint.encode(message_wrapper.packet_type_num)
-        coded_output += varint.encode(message_wrapper.message_type_num)
-        coded_output += varint.encode(self.crypto_type)
-        coded_output += varint.encode(self.connection_id)
-
-        coded_output += message_wrapper.byte_message
-
-        with self.public_kcp_peer.mutex:
-            ret = self.public_kcp_peer.kcp.send(coded_output)
-
-        return ret
+        return False
 
     def recv_header():
         pass
@@ -61,30 +50,47 @@ class RendezvousSession(BaseSession):
 class KcpPeer:
     def __init__(self, sock, ip, port):
 
+        self.peer = Peer(ip, port)
+
         self.kcp = KcpObj(0x11223344, id(self), self)
         self.kcp.nodelay(1, 10, 2, 1)
         self.kcp.wndsize(128, 128)
         self.kcp.setmtu(1400)
 
-        self.next = 0
         self.sock = sock
-
-        self.server_peer = ip, port
-
         self.mutex = threading.Lock()
 
-        self.last_ping = None
+        self.next = 0
+        self.last_ping = get_current_time_sec()
 
     def udp_output(self, data):
-        return self.sock.sendto(data, self.server_peer)
+        return self.sock.sendto(data, (self.peer.ip, self.peer.port))
 
+    def send(self, message_wrapper):
+
+        if not isinstance(message_wrapper, MessageWrapper):
+            return False
+
+        with kcp_peer.mutex:
+            result = kcp_peer.kcp.send(message_wrapper.get_coded_output())
+
+        return result
+
+class Peer:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.key = f'{self.ip}:{self.port}'
+
+    def __eq__(self, other):
+        return self.key == other.key
 
 if __name__ == '__main__':
-    
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.setblocking(False)
 
-    sess = RendezvousSession(sock, 1)
-    sess.set_public_kcp_peer(0x11223344, '127.0.0.1', 9191)
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # sock.setblocking(False)
+
+    # sess = RendezvousSession(sock, 1)
+    # sess.set_public_kcp_peer(0x11223344, '127.0.0.1', 9191)
 
